6a76cde400270fc596db79bfbc5db46cc1967b50
Jan Goralski <jan.wojciech.goralski@gmail.com>
2022-12-06 10:29:14 +0100

    [Maintenance] Support Sylius 1.12

--- tests/Application/config/bundles.php
+++ tests/Application/config/bundles.php
@@ -1,10 +1,11 @@
 <?php
 
-return [
+use Sylius\Bundle\CoreBundle\Application\Kernel as SyliusKernel;
+
+$bundles = [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Sylius\Bundle\OrderBundle\SyliusOrderBundle::class => ['all' => true],
@@ -53,3 +54,9 @@ return [
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true, 'test_cached' => true],
     SyliusLabs\DoctrineMigrationsExtraBundle\SyliusLabsDoctrineMigrationsExtraBundle::class => ['all' => true],
 ];
+
+if (SyliusKernel::VERSION_ID < 11200) {
+    $bundles[Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class] = ['all' => true];
+}
+
+return $bundles;
